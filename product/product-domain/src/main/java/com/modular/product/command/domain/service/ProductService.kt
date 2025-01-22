4ac001f7c63b09668c3635ebd124a1b4934a6ee6
dlwnsgus777 <dlwnsgus777@naver.com>
2025-01-22 10:06:42 +0900

    refactor: 리팩터링

--- product/product-domain/src/main/java/com/modular/product/command/domain/service/ProductService.kt
+++ product/product-domain/src/main/java/com/modular/product/command/domain/service/ProductService.kt
@@ -1,5 +1,6 @@
 package com.modular.product.command.domain.service
 
+import com.modular.product.command.domain.Product
 import com.modular.product.command.domain.repository.ProductRepository
 import com.modular.product.command.domain.service.input.ProductSaveInput
 import com.modular.product.command.domain.service.input.ProductUpdateInput
@@ -16,12 +17,14 @@ class ProductService(
 ) {
 
     fun save(input: ProductSaveInput): ProductSaveOutput {
-        val savedProduct = productRepository.save(input.toEntity(productNamePolicy.parsingProductName(input.productName)))
+        val savedProduct =
+            productRepository.save(input.toEntity(productNamePolicy.parsingProductName(input.productName)))
         return ProductSaveOutput.from(savedProduct)
     }
 
     fun update(productId: Long, input: ProductUpdateInput): ProductUpdateOutput {
-        val product = productRepository.findByIdAndIsDelete(productId, false) ?: throw IllegalArgumentException("상품이 존재하지 않습니다.")
+        val product = getProduct(productId)
+
         input.productName?.let {
             input.productName = productNamePolicy.parsingProductName(it)
         }
@@ -32,7 +35,13 @@ class ProductService(
     }
 
     fun delete(productId: Long) {
-        val product = productRepository.findByIdAndIsDelete(productId, false) ?: throw IllegalArgumentException("상품이 존재하지 않습니다.")
+        val product = getProduct(productId)
         product.delete()
     }
+
+    private fun getProduct(productId: Long): Product {
+        return productRepository.findByIdAndIsDelete(productId, false)
+            ?: throw IllegalArgumentException("상품이 존재하지 않습니다.")
+    }
 }
+
