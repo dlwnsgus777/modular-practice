20b3c6022b21098f612cea940b1e02103b7fd603
dlwnsgus777 <dlwnsgus777@naver.com>
2025-01-22 09:39:37 +0900

    feat: 외부 api 호출 통합 테스트 disabled 처리

--- app/src/test/kotlin/com/modular/product/command/infra/PurgoMalumProfanityFilterTest.kt
+++ app/src/test/kotlin/com/modular/product/command/infra/PurgoMalumProfanityFilterTest.kt
@@ -1,8 +1,7 @@
 package com.modular.product.command.infra
 
 import com.modular.product.command.domain.service.ProfanityFilter
-import com.modular.support.IntegrationTestController
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -17,6 +16,7 @@ class PurgoMalumProfanityFilterTest {
 
     @Test
     @DisplayName("비속어가 포함되어 있으면 오류 발생")
+    @Disabled
     fun filter01() {
         // given
         val name = "fuck"
